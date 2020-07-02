09e2608404deeb6c373c7391aff9c10e1ecb539a
phmf <phmf@cin.ufpe.br>
2020-07-02 16:45:09 -0300

    Hello World

---
+++ Primeiro Programa/MeuPrimeiroPrograma.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // --> printf
+
+/*
+    Meu primeiro programa em C
+    Programa: Escreve no console o texto "Hello World!"
+*/
+
+main()
+{
+
+    //imprimir na tela "Hello World!"
+    printf("Hello World!");
+}
