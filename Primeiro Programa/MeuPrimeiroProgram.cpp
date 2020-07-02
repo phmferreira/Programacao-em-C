6cb2b87c029715a37bc418f62471bb8ff14fe2c7
phmf <phmf@cin.ufpe.br>
2020-07-02 16:55:51 -0300

    Exemplo de Hello World em C++

---
+++ Primeiro Programa/MeuPrimeiroProgram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    Meu primeiro programa em C++
+    Programa: Escreve no console o texto "Hello World!"
+*/
+
+int main()
+{
+    //imprimir na tela "Hello World!"
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
